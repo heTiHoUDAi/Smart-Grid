ca85c4c68058953843d17f4eb88ac488370d1fa6
heTiHoUDAi <30712073+heTiHoUDAi@users.noreply.github.com>
2018-03-07 22:47:55 -0500

    Add gas_error file, Add class in the gas_network

--- GSG/gasSim.py
+++ GSG/gasSim.py
@@ -3,7 +3,55 @@
 Zisheng Wang
 
 The gas network simulation code.
+
+This gas flow and pressure is based on the paper 
+'Solving the Natural Gas Flow Problem using Semidefinite Program Relaxation' 2017
+by A. Ojha, V. Kekatos and R. Baldick.
 """
+
+import gas_errors
+
+class GasNetworkEdge:
+    # @in start, an int indicate the start of the edge
+    # @in end, an int indicate the end of the edge
+    # @in gas_constant, a constant related to the length, diameter, impedence of the 
+    #       pipeline. The definition of it may refer to the 'Simulation and Analysis
+    #       of the Gas Network' page 78, equation 4.28
+    def __init__(self,start,end,gas_constant):
+        if start==end:
+            raise(gas_errors.EdgeStartEndSameError(self))
+        self.start = start
+        self.end   = end
+        self.K     = gas_constant
+    # return the copy of current class
+    def copy(self):
+        import copy
+        return copy.deepcopy(self)
+class GasNetworkCompressor:
+    # @in edge: Class GasNetworkEdge, indicate which pipeline the compressor belongs to.
+    # @in alpha: the compressor ratio. linear multiple the square of the pressure
+    # @in r: the location ratio, represent where is the Compressor
+    def __init__(self, edge, alpha, r):
+        self.edge  = edge.copy()
+        self.alpha = alpha
+        self.r     = r
+    # copy
+    def copy(self):
+        import copy
+        return copy.deepcopy(self)
+    
+
+class GasNetworkModel:
+    # @in nodes: the list of node flows,
+    # @in edges: the list of the edges, the element is a edge class.
+    # @in cps: the list of the compressor, the element is the compressor class
+    def __init__(self, nodes, edges, cps):
+        self.nodes = nodes.copy()
+        self.edges = edges.copy()
+        self.compressor = cps.copy()
+        
 
 def Gas_Network_Simulation():
     print('Hellow')
+    
+a = GasNetworkEdge(1,1,0.1)
---
+++ GSG/gas_errors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  7 22:36:45 2018
+
+@author: Zisheng Wang
+The Error happen in the gas network
+"""
+
+class EdgeStartEndSameError(Exception):
+    def __init__(self,edge):
+        print('Error in '+self.__str__()+' The edge start and end should not be the same')
+        
+
+
